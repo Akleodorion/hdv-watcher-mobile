29b342abe099548740d235fcf44f3f563996221d
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-07-17 23:26:45 +0200

    Created item class, priceType enum

---
+++ hdv_watcher/lib/features/item/domain/entitie/item.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:hdv_watcher/classes/price_class/super_price.dart';
+import 'package:hdv_watcher/enums/ressource_type.dart';
+
+class Item extends Equatable {
+  final String name;
+  final String imgUrl;
+  final RessourceType ressourceType;
+  final int petXp;
+  final int quantityForUnitXp;
+  final SuperPrice superPrice;
+
+  const Item({
+    required this.name,
+    required this.imgUrl,
+    required this.ressourceType,
+    required this.petXp,
+    required this.quantityForUnitXp,
+    required this.superPrice,
+  });
+
+  @override
+  List<Object?> get props => [
+        name,
+        imgUrl,
+        ressourceType,
+        petXp,
+        quantityForUnitXp,
+        superPrice,
+      ];
+}
