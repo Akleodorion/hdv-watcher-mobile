dcce80d37d77e45419240f2366baa85d88f310e2
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-07-20 16:34:06 +0200

    Ajout des pages Item Features et HDV Watcher

---
+++ hdv_watcher/lib/features/item/presentation/pages/hdv_watcher_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class HdvWatcherPage extends StatelessWidget {
+  const HdvWatcherPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- hdv_watcher/lib/features/item/presentation/pages/item_features_page.dart
+++ hdv_watcher/lib/features/item/presentation/pages/item_features_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hdv_watcher/features/item/presentation/pages/hdv_watcher_page.dart';
 
 class ItemFeaturesPage extends StatefulWidget {
   const ItemFeaturesPage({super.key});
@@ -8,15 +9,31 @@ class ItemFeaturesPage extends StatefulWidget {
 }
 
 class _ItemFeaturesPageState extends State<ItemFeaturesPage> {
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: _currentIndex == 0 ? const HdvWatcherPage() : null,
         bottomNavigationBar: BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.currency_exchange), label: "Graph"),
-        BottomNavigationBarItem(icon: Icon(Icons.pets), label: 'Famillier'),
-      ],
-    ));
+          backgroundColor: Colors.grey,
+          currentIndex: _currentIndex,
+          onTap: (value) {
+            setState(() {
+              _currentIndex = value;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.currency_exchange), label: "Graph"),
+            BottomNavigationBarItem(icon: Icon(Icons.pets), label: 'Famillier'),
+          ],
+        ));
   }
 }
