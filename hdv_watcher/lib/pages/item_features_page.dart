39a3938b0b0319b49ca472a46d45e04c1d4ed4db
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-07-24 16:01:51 +0200

    Retrait de la bottom navigation bar et de la feature pet pour le moment.

--- hdv_watcher/lib/pages/item_features_page.dart
+++ hdv_watcher/lib/pages/item_features_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hdv_watcher/features/item/presentation/pages/hdv_watcher/hdv_watcher_page.dart';
-import 'package:hdv_watcher/pages/widgets/my_bottom_navigation_bar.dart';
 
 class ItemFeaturesPage extends StatefulWidget {
   const ItemFeaturesPage({super.key});
@@ -10,33 +9,15 @@ class ItemFeaturesPage extends StatefulWidget {
 }
 
 class _ItemFeaturesPageState extends State<ItemFeaturesPage> {
-  late int _currentIndex;
-
   @override
   void initState() {
     super.initState();
-    _currentIndex = 0;
   }
-
-  void setIndex(value) {
-    setState(() {
-      _currentIndex = value;
-    });
-  }
-
-  final List bodyContent = [
-    const HdvWatcherPage(),
-    null,
-  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: bodyContent[_currentIndex],
-      bottomNavigationBar: MyBottomNavigationBar(
-        onTap: setIndex,
-        index: _currentIndex,
-      ),
+    return const Scaffold(
+      body: HdvWatcherPage(),
     );
   }
 }
