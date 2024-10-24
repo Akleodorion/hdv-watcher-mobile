f4fd5899cb970528444ef8c7462b193d26a9a431
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-10-24 19:56:20 +0200

    Correction sur le fichier item model test

--- hdv_watcher/test/features/item/data/model/item_model_test.dart
+++ hdv_watcher/test/features/item/data/model/item_model_test.dart
@@ -33,73 +33,92 @@ void main() {
       imgUrl: "https://api.dofusdb.fr/img/items/109326.png",
       ressourceType: RessourceType.oeil,
       superPrice: SuperPrice(
-        unitPrices: Prices(prices: [
-          Price(
-              priceValue: 125,
-              priceType: PriceType.unit,
-              scrapDate: DateTime.parse("2024-07-14T15:02:34.954+00:00")),
-          Price(
-              priceValue: 125,
-              priceType: PriceType.unit,
-              scrapDate: DateTime.parse("2024-07-14T17:02:31.617+00:00")),
-          Price(
-              priceValue: 147,
-              priceType: PriceType.unit,
-              scrapDate: DateTime.parse("2024-07-14T18:02:34.031+00:00")),
-          Price(
-              priceValue: 158,
-              priceType: PriceType.unit,
-              scrapDate: DateTime.parse("2024-07-14T20:02:36.610+00:00")),
-          Price(
-              priceValue: 123,
-              priceType: PriceType.unit,
-              scrapDate: DateTime.parse("2024-07-14T22:02:34.727+00:00")),
-        ], priceType: PriceType.unit),
-        tenthPrices: Prices(prices: [
-          Price(
-              priceValue: 1257,
-              priceType: PriceType.tenth,
-              scrapDate: DateTime.parse("2024-07-14T15:02:34.954+00:00")),
-          Price(
-              priceValue: 1257,
-              priceType: PriceType.tenth,
-              scrapDate: DateTime.parse("2024-07-14T17:02:31.617+00:00")),
-          Price(
-              priceValue: 1236,
-              priceType: PriceType.tenth,
-              scrapDate: DateTime.parse("2024-07-14T18:02:34.031+00:00")),
-          Price(
-              priceValue: 1356,
-              priceType: PriceType.tenth,
-              scrapDate: DateTime.parse("2024-07-14T20:02:36.610+00:00")),
-          Price(
-              priceValue: 1554,
-              priceType: PriceType.tenth,
-              scrapDate: DateTime.parse("2024-07-14T22:02:34.727+00:00")),
-        ], priceType: PriceType.tenth),
-        hundredPrices: Prices(prices: [
-          Price(
-              priceValue: 12458,
-              priceType: PriceType.hundred,
-              scrapDate: DateTime.parse("2024-07-14T15:02:34.954+00:00")),
-          Price(
-              priceValue: 12650,
-              priceType: PriceType.hundred,
-              scrapDate: DateTime.parse("2024-07-14T17:02:31.617+00:00")),
-          Price(
-              priceValue: 12385,
-              priceType: PriceType.hundred,
-              scrapDate: DateTime.parse("2024-07-14T18:02:34.031+00:00")),
-          Price(
-              priceValue: 14523,
-              priceType: PriceType.hundred,
-              scrapDate: DateTime.parse("2024-07-14T20:02:36.610+00:00")),
-          Price(
-              priceValue: 13256,
-              priceType: PriceType.hundred,
-              scrapDate: DateTime.parse("2024-07-14T22:02:34.727+00:00")),
-        ], priceType: PriceType.hundred),
+        unitPrices: Prices(
+          prices: [
+            Price(
+                priceValue: 125,
+                priceType: PriceType.unit,
+                scrapDate: DateTime.parse("2024-07-14T15:02:34.954+00:00")),
+            Price(
+                priceValue: 125,
+                priceType: PriceType.unit,
+                scrapDate: DateTime.parse("2024-07-14T17:02:31.617+00:00")),
+            Price(
+                priceValue: 147,
+                priceType: PriceType.unit,
+                scrapDate: DateTime.parse("2024-07-14T18:02:34.031+00:00")),
+            Price(
+                priceValue: 158,
+                priceType: PriceType.unit,
+                scrapDate: DateTime.parse("2024-07-14T20:02:36.610+00:00")),
+            Price(
+                priceValue: 123,
+                priceType: PriceType.unit,
+                scrapDate: DateTime.parse("2024-07-14T22:02:34.727+00:00")),
+          ],
+          priceType: PriceType.unit,
+          capitalGain: 45,
+          medianPrice: 163,
+          currentPrice: 155,
+        ),
+        tenthPrices: Prices(
+          prices: [
+            Price(
+                priceValue: 1257,
+                priceType: PriceType.tenth,
+                scrapDate: DateTime.parse("2024-07-14T15:02:34.954+00:00")),
+            Price(
+                priceValue: 1257,
+                priceType: PriceType.tenth,
+                scrapDate: DateTime.parse("2024-07-14T17:02:31.617+00:00")),
+            Price(
+                priceValue: 1236,
+                priceType: PriceType.tenth,
+                scrapDate: DateTime.parse("2024-07-14T18:02:34.031+00:00")),
+            Price(
+                priceValue: 1356,
+                priceType: PriceType.tenth,
+                scrapDate: DateTime.parse("2024-07-14T20:02:36.610+00:00")),
+            Price(
+                priceValue: 1554,
+                priceType: PriceType.tenth,
+                scrapDate: DateTime.parse("2024-07-14T22:02:34.727+00:00")),
+          ],
+          priceType: PriceType.tenth,
+          capitalGain: 450,
+          medianPrice: 1630,
+          currentPrice: 1554,
+        ),
+        hundredPrices: Prices(
+          prices: [
+            Price(
+                priceValue: 12458,
+                priceType: PriceType.hundred,
+                scrapDate: DateTime.parse("2024-07-14T15:02:34.954+00:00")),
+            Price(
+                priceValue: 12650,
+                priceType: PriceType.hundred,
+                scrapDate: DateTime.parse("2024-07-14T17:02:31.617+00:00")),
+            Price(
+                priceValue: 12385,
+                priceType: PriceType.hundred,
+                scrapDate: DateTime.parse("2024-07-14T18:02:34.031+00:00")),
+            Price(
+                priceValue: 14523,
+                priceType: PriceType.hundred,
+                scrapDate: DateTime.parse("2024-07-14T20:02:36.610+00:00")),
+            Price(
+                priceValue: 13256,
+                priceType: PriceType.hundred,
+                scrapDate: DateTime.parse("2024-07-14T22:02:34.727+00:00")),
+          ],
+          priceType: PriceType.hundred,
+          capitalGain: 4500,
+          medianPrice: 16300,
+          currentPrice: 15540,
+        ),
       ),
+      isLoaded: true,
     );
 
     final List<DateTime> dates = [
@@ -125,11 +144,8 @@ void main() {
       when(mockArrayUtils.getIntsFromArray(hundred))
           .thenReturn([12458, 12650, 12385, 14523, 13256]);
       //act
-      final ItemModel result = ItemModel.fromJson(
-        json: jsonMap,
-        datesUtils: mockDatesUtils,
-        arrayUtils: mockArrayUtils,
-      );
+      final ItemModel result =
+          ItemModel.fromJson(json: jsonMap, isLoaded: true);
       //assert
       expect(result, item1);
     });
