77ebb7e74c6cca6d49aa9d7436d0c0f01bfbdd4e
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-10-26 22:02:59 +0200

    Implémentation du changement. Un élément n'est chargé qu'une fois. Si l'on reclique sur lui est qu'il est loaded alors nous n'avons plus de rechargement.

--- hdv_watcher/lib/features/item/presentation/providers/items/notifiers/fetch_item_notifier.dart
+++ hdv_watcher/lib/features/item/presentation/providers/items/notifiers/fetch_item_notifier.dart
@@ -2,7 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hdv_watcher/core/errors/failures.dart';
 import 'package:hdv_watcher/features/item/domain/entitie/item.dart';
 import 'package:hdv_watcher/features/item/domain/usecase/fetch_item_usecase.dart';
+import 'package:hdv_watcher/features/item/presentation/providers/items/notifiers/items_list_notifier.dart';
+import 'package:hdv_watcher/features/item/presentation/providers/items/providers/unit_items_list_provider.dart';
 import 'package:hdv_watcher/features/item/presentation/providers/items/state/fetch_item_state.dart';
+import 'package:hdv_watcher/features/item/presentation/providers/items/state/item_list_state.dart'
+    as il;
 
 class FetchItemNotifier extends StateNotifier<FetchItemState> {
   final FetchItemUsecase fetchItemsUsecase;
@@ -10,7 +14,12 @@ class FetchItemNotifier extends StateNotifier<FetchItemState> {
   FetchItemNotifier({required this.fetchItemsUsecase}) : super(Unloaded());
   FetchItemState get initialState => Unloaded();
 
-  Future<FetchItemState> fetchItem({required Item item}) async {
+  Future<FetchItemState> fetchItem({
+    required Item item,
+    required WidgetRef ref,
+    required StateNotifierProvider<ItemsListNotifier, il.ItemsListState>
+        provider,
+  }) async {
     state = Loading();
     final response = await fetchItemsUsecase.call(item: item);
 
@@ -22,6 +31,7 @@ class FetchItemNotifier extends StateNotifier<FetchItemState> {
       },
       (success) {
         state = Loaded(item: success);
+        ref.read(provider.notifier).updateItemFromList(newItem: success);
       },
     );
     return state;
