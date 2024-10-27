c12d202093e401af8e8b911e0b0d96cbae0a349f
Christian Bondzie <christian@Christians-MacBook-Air.local>
2024-10-27 16:13:27 +0100

    Création d'un repo notifier

--- hdv_watcher/lib/features/item/presentation/providers/items/notifiers/fetch_item_notifier.dart
+++ hdv_watcher/lib/features/item/presentation/providers/items/notifiers/fetch_item_notifier.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hdv_watcher/core/enums/price_type.dart';
 import 'package:hdv_watcher/core/errors/failures.dart';
+import 'package:hdv_watcher/core/utils/notifier_caller.dart';
 import 'package:hdv_watcher/features/item/domain/entitie/item.dart';
 import 'package:hdv_watcher/features/item/domain/usecase/fetch_item_usecase.dart';
 import 'package:hdv_watcher/features/item/presentation/providers/items/state/fetch_item_state.dart';
@@ -10,10 +12,10 @@ class FetchItemNotifier extends StateNotifier<FetchItemState> {
   FetchItemNotifier({required this.fetchItemsUsecase}) : super(Unloaded());
   FetchItemState get initialState => Unloaded();
 
-  Future<FetchItemState> fetchItem({
-    required Item item,
-    required dynamic provider,
-  }) async {
+  Future<FetchItemState> fetchItem(
+      {required Item item,
+      required NotifierCaller notifier,
+      required PriceType priceType}) async {
     state = Loading();
     final response = await fetchItemsUsecase.call(item: item);
 
@@ -25,7 +27,7 @@ class FetchItemNotifier extends StateNotifier<FetchItemState> {
       },
       (success) {
         state = Loaded(item: success);
-        provider.updateItemFromList(newItem: success);
+        notifier.updateItemFromList(item: item, priceType: priceType);
       },
     );
     return state;
